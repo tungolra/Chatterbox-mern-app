168ed0fdce8b58f1d3b4484206213c580705f3b3
farnaz <farnaz.towhidi@gmail.com>
2022-12-01 17:01:00 -0500

    update api listing

--- src/pages/ListHotelPage/ListHotelPage.jsx
+++ src/pages/ListHotelPage/ListHotelPage.jsx
@@ -3,13 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 
 function ListHotelPage() {
+
+    // console.log (process.env.DATABASE_URL)
     const [hotels, setHotels] = useState([]); 
     const url = 'https://booking-com.p.rapidapi.com/v1/hotels/search?room_number=1&checkin_date=2023-05-27&checkout_date=2023-05-28&units=metric&order_by=popularity&adults_number=2&filter_by_currency=AED&locale=en-gb&dest_id=-553173&dest_type=city&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&children_number=2&page_number=0&include_adjacency=true';
     const options = {
         method: 'GET',
         headers: {
-        'X-RapidAPI-Key': '87e2cbcb43mshd23e9fabe71900dp1a53f1jsn8da71d5f8299',
-        'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+        'X-RapidAPI-Key': `process.env.REACT_APP_RAPID_API_KEY`,
+        'X-RapidAPI-Host': `process.env.REACT_APP_RAPID_API_HOST`
+        // 'X-RapidAPI-Key': '87e2cbcb43mshd23e9fabe71900dp1a53f1jsn8da71d5f8299',
+        // 'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
         }
     };
 
