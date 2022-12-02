d5323a7f9817da9a579b3cbf14431582f0c3f4a4
rtungol <rarrtungol@gmail.com>
2022-12-01 22:35:32 -0500

    message model created, routes set up, create message ctrl function complete

--- controllers/api/chats.js
+++ controllers/api/chats.js
@@ -2,16 +2,12 @@ const Chat = require("../../models/chat");
 
 //create chat
 async function createChat(req, res) {
-  // const newChat = new Chat({
-  //     members: [req.body.senderId, req.body.receiverId]
-  // })
   try {
     const result = await Chat.create({
       members: [req.body.senderId, req.body.receiverId],
     });
     res.status(200).json(result);
   } catch (error) {
-    console.log("hi", error);
     res.status(500).json(error);
   }
 }
