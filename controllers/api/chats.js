c73ca6b4e6e04939a2531e371df8751183b24261
rtungol <rarrtungol@gmail.com>
2022-12-01 22:09:54 -0500

    createChat function complete

--- controllers/api/chats.js
+++ controllers/api/chats.js
@@ -1,20 +1,23 @@
 const Chat = require("../../models/chat");
 
 //create chat
-async function createChat(res, req) {
-    const newChat = new Chat({
-        members: [req.body.senderId, req.body.receiverId]
-    })
-    try {
-        const result = await newChat.save() 
-        res.status(200).json(result)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+async function createChat(req, res) {
+  // const newChat = new Chat({
+  //     members: [req.body.senderId, req.body.receiverId]
+  // })
+  try {
+    const result = await Chat.create({
+      members: [req.body.senderId, req.body.receiverId],
+    });
+    res.status(200).json(result);
+} catch (error) {
+      console.log("hi", error)
+    res.status(500).json(error);
+  }
 }
 
 //show user's chats
-async function showChats(res, req) {}
+async function showChats(req, res) {}
 
 //select from chats
 async function selectChat(req, res) {}
