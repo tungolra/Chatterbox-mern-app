0ec8cda4ab7acf1a766411d5236b09c4cc0138bd
rtungol <rarrtungol@gmail.com>
2022-12-03 00:12:31 -0500

    useeffect on chatlist to get chat data from db complete, render chats on page

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatBox from "../ChatBox/ChatBox";
+import * as chatsService from "../../utilities/ChatRequests/chat-service";
+import axios from "axios";
 
-export default function ChatList() {
+export default function ChatList({ user }) {
+  const [chats, setChats] = useState([]);
+  // const [onlineUsers, setOnlineUsers] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [sendMessage, setSendMessage] = useState(null);
+  const [receivedMessage, setReceivedMessage] = useState(null);
+
+  //get chat
+  useEffect(() => {
+    const getUserChats = async () => {
+      try {
+        let response = await axios.get(`/api/chats/${user._id}`);
+        // console.log("Response", response)
+        // if (!response.ok) throw new Error("No response received")
+        let chatsData = response;
+        setChats(chatsData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUserChats();
+  }, [user._id]);
+
+
   return (
     <>
-      <div style={{border: "1px solid black"}}>
+      <div style={{ border: "1px solid black" }}>
         This ChatList gets data from DB of user's current convos
         <ul>
-          <li>Convo #1 </li>
+          {/* need to show name of person chatting to */}
+          {chats.map((chat, idx) => (
+            <li key={idx}
+             onClick={() => setCurrentChat(chat)}
+             >Chat member: {chat.members[1]} </li>
+          ))}
           <li>Convo #2 </li>
           <li>Convo #3 </li>
           <li>Convo #... </li>
         </ul>
       </div>
       <div> Convo selected ? show Chatbox : ""</div>
-      <ChatBox/>
+      <ChatBox />
     </>
   );
 }
