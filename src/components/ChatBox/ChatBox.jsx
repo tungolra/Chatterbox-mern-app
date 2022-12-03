415e26227fd743df06854ef3d1b1c7e402cf4c50
rtungol <rarrtungol@gmail.com>
2022-12-03 11:34:11 -0500

    ternary on chatbox component to only show chatbox when conversation is selected

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Messages from "../Messages/Messages";
 import InputEmoji from "react-input-emoji";
 import { format } from "timeago.js";
-import axios from "axios"
+import axios from "axios";
 
 export default function ChatBox({
   currentChat,
@@ -15,45 +15,50 @@ export default function ChatBox({
   const [newMessage, setNewMessage] = useState("");
 
   // add received message to list of messages
-  useEffect(() => { 
-    if (receivedMessage !== null && receivedMessage?.chatId == currentChat?._id){
-      setMessages([...messages, receivedMessage])
+  useEffect(() => {
+    if (
+      receivedMessage !== null &&
+      receivedMessage?.chatId == currentChat?._id
+    ) {
+      setMessages([...messages, receivedMessage]);
     }
-  }, [receivedMessage])
-
+  }, [receivedMessage]);
 
   // get chat member data
-  useEffect(() => { 
-    const userId = currentChat?.members?.find((id) => id !== currentUserId) 
-    setUserData(userId)
-  }, [currentChat, currentUserId])
+  useEffect(() => {
+    const userId = currentChat?.members?.find((id) => id !== currentUserId);
+    setUserData(userId);
+  }, [currentChat, currentUserId]);
 
   // get messages for chat
-  useEffect(()=> {
-    const serverRoute = "api/messages"
-    const getChatMessages = async () => { 
+  useEffect(() => {
+    const serverRoute = "api/messages";
+    const getChatMessages = async () => {
       try {
-        let response = await axios.get(`${serverRoute}/${currentChat._id}`)
-        setMessages(response.data)
+        let response = await axios.get(`${serverRoute}/${currentChat._id}`);
+        setMessages(response.data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    if (currentChat!== null) getChatMessages()
-  }, [currentChat])
-
+    };
+    if (currentChat !== null) getChatMessages();
+  }, [currentChat]);
 
   function handleChange(e) {}
   async function handleSend(e) {}
   return (
     <>
-      <div style={{ border: "1px solid black" }}>
-        This ChatBox will render the container for a conversation the user
-        selects
-        <hr />
-        <Messages messages={messages}/>
-        <InputEmoji value="" onChange={handleChange} />
-      </div>
+      {currentChat ? (
+        <div style={{ border: "1px solid black" }}>
+          This ChatBox will render the container for a conversation the user
+          selects
+          <hr />
+          <Messages messages={messages} />
+          <InputEmoji value="" onChange={handleChange} />
+        </div>
+      ) : (
+        <span>Click a Chat to Start Conversation</span>
+      )}
     </>
   );
 }
