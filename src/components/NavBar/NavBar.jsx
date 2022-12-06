542cbc0723579f2830e51e694a03ca64f359c391
Kendra <kendrayoshizawa@gmail.com>
2022-12-06 09:15:09 -0800

    added grid columns to page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as userService from "../../utilities/UserRequests/users-service";
-import { AppBar } from "@mui/material";
-import Button from "@mui/material/Button";
+import { AppBar, Grid } from "@mui/material";
 
 export default function NavBar(props) {
   function handleLogOut() {
@@ -10,27 +9,31 @@ export default function NavBar(props) {
     props.setUser(null);
   }
   return (
-    <AppBar
-      boxShadow="none"
-      position="static"
-      color="transparent"
-      sx={{ zIndex: "0" }}
-    >
-      <nav style={{ border: "1px solid blue" }}>
-        {" "}
-        {/* Add own links */}
-        <h1 className="logo">chatter[box]</h1>
-        <br></br>
-        <Link color="secondary" to="/">
-          Home&nbsp;
-        </Link>
-        <Link to="/chats">Chats&nbsp;</Link>
-        <Link to="" onClick={handleLogOut}>
-          Log Out&nbsp;
-        </Link>
-        <br></br>
-        {props.user && <span>Welcome, {props.user?.firstname}!</span>}
-      </nav>
-    </AppBar>
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <AppBar
+          boxShadow="none"
+          position="static"
+          color="transparent"
+          sx={{ zIndex: "0" }}
+        >
+          <nav style={{ border: "1px solid blue" }}>
+            {" "}
+            {/* Add own links */}
+            <h1 className="logo">chatter[box]</h1>
+            <br></br>
+            <Link color="secondary" to="/">
+              Home&nbsp;
+            </Link>
+            <Link to="/chats">Chats&nbsp;</Link>
+            <Link to="" onClick={handleLogOut}>
+              Log Out&nbsp;
+            </Link>
+            <br></br>
+            {props.user && <span>Welcome, {props.user?.firstname}!</span>}
+          </nav>
+        </AppBar>
+      </Grid>
+    </Grid>
   );
 }
