df86fd7d4aadc841b29ac2f0e6c632f7fed89727
farnaz <farnaz.towhidi@gmail.com>
2022-12-07 22:38:46 -0500

    finished update styling in update page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import UpdateUserModal from "../../components/UpdateUserModal/UpdateUserModal";
-
+import { Input, Grid } from "@mui/material";
 
 export default function Home({user,setUser}) {
   const [modalOpened, setModalOpened] = useState(false);
  
   return (
     <>
+    
       <div>Home Page</div>
-      <div>
+      <div className="container">
         <button onClick={() => setModalOpened(true)}>Edit Profile</button>
         <UpdateUserModal
           modalOpened={modalOpened}
