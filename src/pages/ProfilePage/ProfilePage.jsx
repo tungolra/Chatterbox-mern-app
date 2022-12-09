e9f670d5213408129f96911e314789ff5c2532cc
Kendra <kendrayoshizawa@gmail.com>
2022-12-08 22:59:36 -0800

    profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,70 @@
 import { useState } from "react";
 import UpdateUserModal from "../../components/UpdateUserModal/UpdateUserModal";
-import { Button, IconButton, Link } from "@mui/material";
+import { Button, IconButton, Link, Grid } from "@mui/material";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import Stack from "@mui/material/Stack";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import NavBar from "../../components/NavBar/NavBar";
 
-export default function ProfilePage({user,setUser}) {
+export default function ProfilePage({ user, setUser }) {
   const [modalOpened, setModalOpened] = useState(false);
   return (
     <>
-      <div>Profile Page</div>
-      <div>
-        <Stack>
-          <div>{user?.firstname}</div>
-          <div>{user?.lastname}</div>
-          <div>{user?.email}</div>
-          <div>{user?.profilePicture} PROFILE PICTURE HERE</div>
-          <div>{user?.about} BIO HERE</div>
-        </Stack>
-        <IconButton>
-          <Link onClick={() => setModalOpened(true)}>
-            <SettingsOutlinedIcon color="primary"></SettingsOutlinedIcon>
-          </Link>
-        </IconButton>
-        <UpdateUserModal
-          modalOpened={modalOpened}
-          setModalOpened={setModalOpened}
-          user={user}
-          setUser={setUser}
-        />
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xl={1}></Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <div>
+            <NavBar user={user} setUser={setUser} />
+          </div>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={8}
+          lg={6}
+          xl={6}
+          sx={{
+            // justifyContent: "center",
+            // alignContent: "center",
+            // alignItems: "center",
+            margin: "auto",
+            marginTop: "10vh",
+            // height: "50px",
+          }}
+        >
+          <Button
+            sx={{
+              // padding: "20px",
+              justifyContent: "right",
+              alignContent: "right",
+            }}
+          >
+            <Link>
+              <SettingsOutlinedIcon
+                color="primary"
+                onClick={() => setModalOpened(true)}
+              ></SettingsOutlinedIcon>
+            </Link>
+            <div>Edit Profile</div>
+          </Button>
+          <div
+            style={{
+              margin: "10px",
+              alignContent: "center",
+            }}
+          >
+            <ProfileCard user={user} />
+          </div>
+          <UpdateUserModal
+            modalOpened={modalOpened}
+            setModalOpened={setModalOpened}
+            user={user}
+            setUser={setUser}
+          />
+        </Grid>
+        <Grid item xl={1}></Grid>
+      </Grid>
     </>
   );
 }
